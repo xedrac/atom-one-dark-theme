3d2a54a7fe00f9a0828b81e0459dc83701f29979
Shawn Badger <shawnbadger@gmail.com>
2026-07-20 19:49:08 -0600

    Try a darker background

--- atom-one-dark-theme.el
+++ atom-one-dark-theme.el
@@ -33,9 +33,11 @@
 (defvar atom-one-dark-colors-alist
   (let* ((256color  (eq (display-color-cells (selected-frame)) 256))
          (colors `(("atom-one-dark-accent"   . "#528BFF")
-                   ("atom-one-dark-fg"       . (if ,256color "color-248" "#ABB2BF"))
+                   ;("atom-one-dark-fg"       . (if ,256color "color-248" "#ABB2BF"))
+                   ("atom-one-dark-fg"       . (if ,256color "color-248" "#8B828F"))
                    ;("atom-one-dark-bg"       . (if ,256color "color-235" "#282C34"))
-                   ("atom-one-dark-bg"       . (if ,256color "color-235" "#232326"))
+                   ;("atom-one-dark-bg"       . (if ,256color "color-235" "#232326"))
+                   ("atom-one-dark-bg"       . (if ,256color "color-235" "#121314"))
                    ("atom-one-dark-bg-1"     . (if ,256color "color-234" "#121417"))
                    ("atom-one-dark-bg-hl"    . (if ,256color "color-236" "#2C323C"))
                    ("atom-one-dark-gutter"   . (if ,256color "color-239" "#4B5363"))
